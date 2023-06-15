0a15dcbdd0243e6b19bdc5d4caf1f4305b9a0a2a
Alvaro-LGPA <alvarolaura@gmail.com>
2023-06-15 15:00:52 +0200

    Commit SearchBar file that was falling out of Git reach

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,12 @@
+import testButton from "../../util/spotify";
 import "./SearchBar.css"
 
 function SearchBar() {
     return (
         <>
             <div className="SearchBar">
-                <input type="text" placeholder="Enter A Song, Album, or Artist"></input>
-                <button className="SearchButton">SEARCH</button>
+                <input type="text" id="search-query" placeholder="Enter A Song, Album, or Artist"></input>
+                <button onClick={testButton} className="SearchButton" id="search">SEARCH</button>
             </div>
         </>
     )
